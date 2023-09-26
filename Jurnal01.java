1df0fb9f0033994e23870cdcc91e8883292e1b62
salmaazzz <azizahsalma2304@gmail.com>
2023-09-26 17:51:44 +0700

    Initial commit

---
+++ Jurnal01.java
@@ -0,0 +1,8 @@
+public class Jurnal01 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Salma Nur Azizah");
+        System.out.println("NIM: 607062300040");
+        System.out.println("Kelas: D3-IF-47-03");
+    }
+}
